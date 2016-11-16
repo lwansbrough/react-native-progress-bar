65e16992810b8a2672125d758ee02d864a8cc966
yasemincidem <yasemincidem@gmail.com>
2016-11-16 11:26:47 +0300

    Flexbox used instead of fixed width so progressbar will be flexible.

--- ProgressBar.js
+++ ProgressBar.js
@@ -8,60 +8,66 @@ import {
 } from 'react-native';
 
 var styles = StyleSheet.create({
-  background: {
-    backgroundColor: '#bbbbbb',
-    height: 5,
-    overflow: 'hidden'
-  },
-  fill: {
-    backgroundColor: '#3b5998',
-    height: 5
-  }
+    background: {
+        backgroundColor: '#bbbbbb',
+        height: 5,
+        overflow: 'hidden'
+    },
+    fill: {
+        backgroundColor: '#3b5998',
+        height: 5
+    }
 });
 
 var ProgressBar = React.createClass({
 
-  getDefaultProps() {
-    return {
-      style: styles,
-      easing: Easing.inOut(Easing.ease),
-      easingDuration: 500
-    };
-  },
+    getDefaultProps() {
+        return {
+            style: styles,
+            easing: Easing.inOut(Easing.ease),
+            easingDuration: 500
+        };
+    },
 
-  getInitialState() {
-    return {
-      progress: new Animated.Value(this.props.initialProgress || 0)
-    };
-  },
+    getInitialState() {
+        return {
+            progress: new Animated.Value(this.props.initialProgress || 0),
+            calculatedWidth: 0
+        };
+    },
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.props.progress >= 0 && this.props.progress != prevProps.progress) {
-      this.update();
+    componentDidUpdate(prevProps, prevState) {
+        if (this.props.progress >= 0 && this.props.progress != prevProps.progress) {
+            this.update();
+        }
+    },
+
+    render() {
+        var fillWidth = this.state.progress.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0 * (this.props.style.width || this.state.calculatedWidth ),
+                1 * (this.props.style.width || this.state.calculatedWidth)],
+        });
+
+        return (
+            <View style={[styles.background, this.props.backgroundStyle, this.props.style]}
+                  onLayout={(event) => {
+                      if (!this.props.style.width) {
+                          this.setState({calculatedWidth: event.nativeEvent.layout.width})
+                      }
+                  }}>
+                <Animated.View style={[styles.fill, this.props.fillStyle, {width: fillWidth}]}/>
+            </View>
+        );
+    },
+
+    update() {
+        Animated.timing(this.state.progress, {
+            easing: this.props.easing,
+            duration: this.props.easingDuration,
+            toValue: this.props.progress
+        }).start();
     }
-  },
-
-  render() {
-
-    var fillWidth = this.state.progress.interpolate({
-      inputRange: [0, 1],
-      outputRange: [0 * this.props.style.width, 1 * this.props.style.width],
-    });
-
-    return (
-      <View style={[styles.background, this.props.backgroundStyle, this.props.style]}>
-        <Animated.View style={[styles.fill, this.props.fillStyle, { width: fillWidth }]}/>
-      </View>
-    );
-  },
-
-  update() {
-    Animated.timing(this.state.progress, {
-      easing: this.props.easing,
-      duration: this.props.easingDuration,
-      toValue: this.props.progress
-    }).start();
-  }
 });
 
 module.exports = ProgressBar;
